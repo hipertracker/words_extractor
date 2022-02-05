797fe6d3b4ec4da101ac3f5fb12b0cd1c4bd4774
Jaroslaw Zabiello <hipertracker@gmail.com>
2022-02-05 23:38:26 +0000

    update Ruby example

--- example-ruby/words.rb
+++ example-ruby/words.rb
@@ -11,19 +11,21 @@ Dir.mkdir(outdir)
 
 sorted = false
 
-paths = Dir['../data/pl/**/*.yml']
+paths = Dir['../data/??/**/*.yml']
+total_size = 0
+count = paths.count
 
-Parallel.each(paths, in_processes: Etc.nprocessors) do |yaml_path| 
+Parallel.each_with_index(paths, in_processes: Etc.nprocessors) do |yaml_path, i|
   meta = YAML.load_file(yaml_path)
   words = IO.read(yaml_path.gsub('.yml', '.txt')).downcase.strip.split(/[^\p{word}]+/).uniq
-  if sorted
-    words = words.sort
-  end
+  words = words.sort if sorted
+  total_size += words.count
   outpath = "#{outdir}/#{meta['lang']}-#{meta['code']}.txt"
-  puts outpath
+  # puts "[#{i}/#{count}] #{yaml_path}/#{outpath}"
+  puts(format('[%3d/%d] %s/%s', i, count, yaml_path, outpath))
   File.write(outpath, words.join("\n"))
 end
 
 secs = Time.now - start
+puts "Total size: #{total_size}"
 puts "Total time: #{secs} s"
-
