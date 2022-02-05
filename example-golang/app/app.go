4a76fa2ff0257fbfff9d35c2de51f034c6ec82ab
Roman Kulish <roman.kulish@news.com.au>
2022-02-05 22:20:05 +1100

    Golang example refactoring.

---
+++ example-golang/app/app.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+const dirPerms = 0755
+
+type empty struct{}
+
+// Run extracts unique words from the list of files and saves them to the outDir.
+// No error handling, no context cancellation is implemented to match implementations
+// in other languages.
+func Run(srcDir, outDir string, numWorkers int, sortResults bool) error {
+	files, err := filepath.Glob(srcDir)
+	if err != nil {
+		return fmt.Errorf(`app: getting list of files "%s": %w`, srcDir, err)
+	}
+
+	if err = clearOutput(outDir); err != nil {
+		return err
+	}
+
+	// This is a very basic semaphore implementation. Counting unique words from
+	// a stream of data is IO, memory and CPU expensive. Semaphore lets to run
+	// up to the numWorkers or workers concurrently and, by default, this number
+	// matches the number of CPUs.
+	sem := make(chan empty, numWorkers)
+
+	var wg sync.WaitGroup
+	var spec *MetaConfig
+
+	for _, file := range files {
+		sem <- empty{}
+
+		if spec, err = ReadSpec(file); err != nil {
+			return err
+		}
+
+		src := file[:len(file)-3] + "txt"
+		dst := filepath.Join(outDir, "extracted-words-for-"+spec.Code+".txt")
+
+		wg.Add(1)
+		go extract(src, dst, "POLISH_CI", sortResults, sem, &wg)
+	}
+
+	wg.Wait()
+	close(sem)
+
+	return nil
+}
+
+func clearOutput(outDir string) error {
+	if err := os.RemoveAll(outDir); err != nil {
+		return fmt.Errorf(`app: cleaning previous results in "%s": %w`, outDir, err)
+	}
+	if err := os.MkdirAll(outDir, dirPerms); err != nil {
+		return fmt.Errorf(`app: creating output directory "%s": %w`, outDir, err)
+	}
+
+	return nil
+}
--- example-golang/main.go
+++ example-golang/main.go
@@ -1,169 +1,55 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"regexp"
-	"sort"
-	"strings"
-	"sync"
+	"path/filepath"
+	"runtime"
 	"time"
 
-	"github.com/bmatcuk/doublestar"
-	"github.com/thoas/go-funk"
-	"github.com/tidwall/collate"
+	"github.com/hipertracker/word_extractor/app"
 )
 
-type Pair struct {
-	SrcPath string
-	Dstpath string
-}
-
-var srcPath = "../data/??/**/*.yml"
-var outdir = "words"
-
-var wg sync.WaitGroup
-
-// func mainOld() {
-// 	paths, _ := doublestar.Glob(srcPath)
-
-// 	clearResults()
-// 	runWithChannels(paths)
-
-// 	clearResults()
-// 	runWithWaitGroups(paths)
-// }
+const (
+	srcPath = "../data/??/**/*.yml"
+	outDir  = "words"
+)
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-
-		t := time.Now()
-		defer timeTrack(t)
-
-		paths, _ := doublestar.Glob(srcPath)
-
-		ch1 := make(chan Pair, len(paths))
-		ch2 := make(chan string, len(paths))
-
-		clearResults()
-
-		for _, yamlPath := range paths {
-			go loadYaml(ch1, yamlPath)
-		}
-
-		for range paths {
-			pair := <-ch1
-			go loadText(ch2, pair.SrcPath, pair.Dstpath, true)
-		}
-		for range paths {
-			fmt.Printf("Saved %s\n", <-ch2)
-		}
-	}()
-	wg.Wait()
-}
-
-func loadYaml(ch chan Pair, path string) {
-	meta := GetYAML(path)
-	srcPath := strings.Replace(path, ".yml", ".txt", -1)
-	dstPath := fmt.Sprintf("%s/extracted-words-for-%s.txt", outdir, meta.Code)
-	ch <- Pair{srcPath, dstPath}
-}
-
-func loadText(ch2 chan string, srcPath string, dstPath string, sorting bool) {
-	content, err := ioutil.ReadFile(srcPath)
+	wd, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		_, _ = fmt.Fprintf(os.Stderr, "Cannot get working directory: %s", err)
+		os.Exit(1)
 	}
-	words := extractUniqueWords(content)
-	if sorting {
-		words = sortWords(words, "POLISH_CI")
-	}
-	text := strings.Join(words, "\n")
-	for err := ioutil.WriteFile(dstPath, []byte(text), 0644); err != nil; {
-		panic(err)
-	}
-	ch2 <- dstPath
-}
 
-func clearResults() {
-	os.RemoveAll(outdir)
-	os.Mkdir(outdir, 0777)
-}
+	defaultNumWorkers := runtime.NumCPU()
 
-func runWithChannels(paths []string) {
-	var ch = make(chan string)
+	// In the original Go code, results where always sorted, unlike in Rust or
+	// Python implementations. Sorting is turned off, by default.
+	var sortResults bool
+	var numWorkers int
+
+	flag.IntVar(&numWorkers, "n", defaultNumWorkers, "Number of workers to run (zero to match the number of available CPUs)")
+	flag.BoolVar(&sortResults, "s", sortResults, "Sort results")
+	flag.Parse()
+
+	if numWorkers <= 0 {
+		numWorkers = defaultNumWorkers
+	}
+
 	t := time.Now()
-	defer timeTrack(t)
-	for _, path := range paths {
-		go func(yamlPath string) {
-			ch <- parseFile(yamlPath, false)
-		}(path)
-	}
-	for range paths {
-		<-ch
-	}
-}
 
-func runWithWaitGroups(paths []string) {
-	var wg sync.WaitGroup
-	t := time.Now()
-	defer timeTrack(t)
-	for _, path := range paths {
-		wg.Add(1)
-		go func(yamlPath string) {
-			parseFile(yamlPath, false)
-			wg.Done()
-		}(path)
-	}
-	wg.Wait()
-}
-
-func parseFile(path string, sorting bool) string {
-	// load YAML file
-	meta := GetYAML(path)
-	outfilepath := fmt.Sprintf("%s/extracted-words-for-%s.txt", outdir, meta.Code)
-
-	// load text file
-	filepath := strings.Replace(path, ".yml", ".txt", -1)
-	content, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		panic(err)
+	var exitCode int
+	if err = app.Run(filepath.Join(wd, srcPath), filepath.Join(wd, outDir), numWorkers, sortResults); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Shit happened: %s", err)
+		exitCode = 1
 	}
 
-	words := extractUniqueWords(content)
-
-	// sort unique words
-	if sorting {
-		words = sortWords(words, "POLISH_CI")
-	}
-
-	text := strings.Join(words, "\n")
-	for err := ioutil.WriteFile(outfilepath, []byte(text), 0644); err != nil; {
-		panic(err)
-	}
-	return outfilepath
+	timeTrack(t)
+	os.Exit(exitCode)
 }
 
 func timeTrack(start time.Time) {
 	fmt.Println("Total timing: ", time.Since(start))
-}
-
-func extractUniqueWords(content []byte) []string {
-	text := strings.ToLower(string(content))
-	re := regexp.MustCompile(`[^\p{L}]+`)
-	tokens := re.Split(text, -1)
-	return funk.UniqString(tokens)
-}
-
-func sortWords(words []string, lang string) []string {
-	less := collate.IndexString(lang)
-	sort.SliceStable(words, func(i, j int) bool {
-		return less(words[i], words[j])
-	})
-	return words
 }
