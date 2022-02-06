f2a42e0463db3a07a6caafa942ec50ac06a91d60
Jaroslaw Zabiello <hipertracker@gmail.com>
2022-02-06 01:44:10 +0000

    update Ruby example

--- example-ruby/words.rb
+++ example-ruby/words.rb
@@ -1,31 +1,31 @@
 require 'yaml'
+require 'yaml'
 require 'parallel'
 require 'etc'
 require 'fileutils'
 
 outdir = 'words'
-start = Time.now
 
 FileUtils.rm_rf(outdir)
 Dir.mkdir(outdir)
 
+t = Time.now
+
 sorted = false
 
 paths = Dir['../data/??/**/*.yml']
-total_size = 0
 count = paths.count
 
-Parallel.each_with_index(paths, in_processes: Etc.nprocessors) do |yaml_path, i|
+sizes = Parallel.map_with_index(paths, in_processes: Etc.nprocessors) do |yaml_path, i|
   meta = YAML.load_file(yaml_path)
-  words = IO.read(yaml_path.gsub('.yml', '.txt')).downcase.strip.split(/[^\p{word}]+/).uniq
+  filepath = yaml_path.gsub('.yml', '.txt')
+  words = IO.read(filepath).downcase.strip.split(/[^\p{word}]+/).uniq
   words = words.sort if sorted
-  total_size += words.count
   outpath = "#{outdir}/#{meta['lang']}-#{meta['code']}.txt"
-  # puts "[#{i}/#{count}] #{yaml_path}/#{outpath}"
   puts(format('[%3d/%d] %s/%s', i, count, yaml_path, outpath))
   File.write(outpath, words.join("\n"))
+  File.size(filepath)
 end
 
-secs = Time.now - start
-puts "Total size: #{total_size}"
-puts "Total time: #{secs} s"
+puts "Total size: #{(sizes.sum / 1024.0 / 1024).round} MB"
+puts "Total time: #{Time.now - t} s"
