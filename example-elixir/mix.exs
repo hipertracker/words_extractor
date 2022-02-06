29e7b85b42ef4ba8577ac3b824e69c45dd792e8f
Jaroslaw Zabiello <hipertracker@gmail.com>
2022-02-06 01:48:42 +0000

    update Elixir code

--- example-elixir/mix.exs
+++ example-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule WordsExtractor.MixProject do
   def project do
     [
       app: :words_extractor_ex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,7 +21,8 @@ defmodule WordsExtractor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:yaml_elixir, "~> 2.8"}
+      {:yaml_elixir, "~> 2.8"},
+      {:timex, "~> 3.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
